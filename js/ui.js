f9d2893f97e35324d2a35dc63cf8fcfd73f027cd
Monggoman <44967832+Monggoman@users.noreply.github.com>
2021-12-13 16:46:09 +0800

    Update 1 - 12-13
    add manifest and service worker

--- js/ui.js
+++ js/ui.js
@@ -1,42 +1,9 @@
-$(document).ready(function(){
-    $('.modal').modal();
-    $('.dropdown-trigger').dropdown();
- });
-
- const quiz= document.querySelector('Average_of_Quiz-input');
- const act= document.querySelector('Average_of_Activities-input');
- const ass= document.querySelector('Average_of_Assignments-input');
-
- const ctx = document.getElementById('myChart').getContext('2d');
- let myChart = new Chart(ctx,{
-
-    type: 'pie',
-    data: {
-        labels : ['Average Quizes', 'Average Activities', 'Average Assignments'],
-        datasets : [
-            {
-                label: '# of votes',
-                data: [0,0,0],
-                backgroundColor: ['#2adece','#dd3b78', '#ff766b'],
-                borderWidth: 1
-            }
-        ]
-    }
-
- });
-
- const updateChartValue = (input, dataOrder)=> {
-
-    input.addEventListener( 'change', e =>{
-        myChart.data.dataset[0].data[dataOrder] = e.target.value;
-        myChart.update();    
-    });
-
- };
-
- updateChartValue(quiz, 0);
- updateChartValue(act, 1);
- updateChartValue(ass, 2);
-
- 
+document.addEventListener('DOMContentLoaded', function() {
+    // nav menu
+    const menus = document.querySelectorAll('.side-menu');
+    M.Sidenav.init(menus, {edge: 'right'});
+    // add recipe form
+    const forms = document.querySelectorAll('.side-form');
+    M.Sidenav.init(forms, {edge: 'left'});
+  });
 
