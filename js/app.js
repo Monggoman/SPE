65fb8c05c7608ec13ac236404bd755c97a50d69d
Monggoman <44967832+Monggoman@users.noreply.github.com>
2021-12-16 19:57:49 +0800

    Big up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('https://monggoman.github.io/SPE.github.io/sw.js')
         .then((reg) => console.log('server worker registered'))
         .catch((err) => console.log('service worker not registered'))
 }
