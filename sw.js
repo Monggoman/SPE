e7d48f790b17534212e228bd83ae66e4e35bc9fa
Monggoman <44967832+Monggoman@users.noreply.github.com>
2021-12-16 21:38:53 +0800

    Big update Offline Function

--- sw.js
+++ sw.js
@@ -1,13 +1,53 @@
+const staticCacheName = "site-static-v1";
+const dynamicCache = "dynamic-v1";
+const assets = [
+    'https://monggoman.github.io/SPE.github.io/',
+    'https://monggoman.github.io/SPE.github.io/index.html',
+    'https://monggoman.github.io/SPE.github.io/js/app.js',
+    'https://monggoman.github.io/SPE.github.io/js/materialize.min.js',
+    'https://monggoman.github.io/SPE.github.io/js/ui.js',
+    'https://monggoman.github.io/SPE.github.io/css/style.css',
+    'https://monggoman.github.io/SPE.github.io/css/materialize.min.css',
+    'https://monggoman.github.io/SPE.github.io/img/logo1.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 //install service worker
 self.addEventListener('install', evt=>{
-    console.log('service worker has been installed');
+    //console.log('service worker has been installed');
+    evt.waitUntil(
+    caches.open(staticCacheName).then(cache =>{
+        console.log('caching shell assets')
+        cache.addAll(assets)
+    }));
 });
 
 // activate event
 self.addEventListener('activate', evt =>{
-    console.log('service worker has been activated')
+    //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys =>{
+            //console.log(keys);
+            return Promise.all(keys
+                    .filter(key => key !== staticCacheName)
+                    .map (key => caches.delete(key))
+                )
+        })
+    );
 });
+
 //fetch event
 self.addEventListener('fetch', evt =>{
-    console.log('fetch event', evt);
+    //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes =>{
+            return cacheRes || fetch(evt.request).then(fetchRes =>{
+                return caches.open(dynamicCache).then(cache =>{
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            })
+        })
+    );
 });
+
